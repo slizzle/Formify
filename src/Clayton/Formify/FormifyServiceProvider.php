3adf7f57141d905d96ebafc2e50959fd87a61ec7
clayton <cks.salazar@gmail.com>
2014-05-10 10:48:58 -0500

    Initial commit

---
+++ src/Clayton/Formify/FormifyServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Clayton\Formify;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormifyServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('clayton/formify');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
